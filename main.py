e6fde9f3568513f2a6123d8d77b4f9235a6f1c8b
Kacper Sieradzinski <ksieradzinski@gmail.com>
2023-11-21 22:41:10 +0100

    database initialization

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import Config
 from products.main import products
+from database import init_db, init_app
 
 app = Flask(__name__)
 app.config.from_object(Config)
+init_app(app)
 app.register_blueprint(products, url_prefix="/products")
