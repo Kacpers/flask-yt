0a82a9fa1d8d72ffad82dcbd8ea82d879f490390
Kacper Sieradzinski <ksieradzinski@gmail.com>
2023-11-21 22:16:17 +0100

    configuration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from config import Config
 from products.main import products
 
 app = Flask(__name__)
+app.config.from_object(Config)
 app.register_blueprint(products, url_prefix="/products")
-
